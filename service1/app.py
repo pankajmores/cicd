f09377d884e68a63d356e2092bb5163af6413dd2
Pankaj More <pankajmore@Pankajs-MacBook-Air.local>
2025-10-08 10:59:52 +0530

    Initial commit

---
+++ service1/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Service 1"
+
+@app.route("/call-service2")
+def call_service2():
+    resp = requests.get("http://service2:5000/")
+    return f"Service1 calling -> {resp.text}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
